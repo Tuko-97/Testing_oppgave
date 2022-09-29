0fe527d79d963cf3f44c775f0f0ce30e4083089c
Tuko-97 <57348795+Tuko-97@users.noreply.github.com>
2022-09-29 12:15:44 +0200

    Update
    an update

--- quizz.js
+++ quizz.js
@@ -1,12 +1,12 @@
 import questions from './questions.json' assert {type: "json"};
 
-export const testQuizz = (guess, quizzNumber) => {
+export const testQuizz = (answer, quizzNumber) => {
     const myQuestions = questions.questions[quizzNumber];
     const myAnswers = myQuestions.myAnswers;
     let message = "Sorry, you were wrong! Please try again";
 
     myAnswers.map((myAnswer) => {
-        if (myAnswer.myAnswer.toLowerCase() === guess.toLowerCase()) {
+        if (myAnswer.myAnswer.toLowerCase() === answer.toLowerCase()) {
             console.log(myAnswer.correct);
             message = "You are right!";
         } else {
